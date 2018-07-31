b2ad3541d3938d842f7a15a2a24db1dd1c1f6c35
yaosong5 <yaosong5@qq.com>
2018-07-31 14:19:15 +0800

    Site updated: 2018-07-31 14:19:14

--- js/main.js
+++ js/main.js
@@ -1,126 +1,204 @@
-(function($){
-    // Caption
-    $('.article-entry').each(function(i) {
-        $(this).find('img').filter(function (element) {
-            return $(this).hasClass('');
-        }).each(function() {
-            // add image caption
-            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
-                $(this).after('<span class="caption">' + this.alt + '</span>');
-            }
+require([], function (){
 
-            if ($(this).parent().prop("tagName") !== 'A') {
-                $(this).wrap('<a href="' + ($(this).attr("data-imgbig") ? $(this).attr("data-imgbig") : this.src) + '" title="' + this.alt + '" class="gallery-item"></a>');
-            }
-        });
-    });
-    if (typeof lightGallery != 'undefined') {
-        var options = {
-            selector: '.gallery-item'
-        };
-        $('.article-entry').each(function(i, entry) {
-            lightGallery(entry, options);
-        });
-        lightGallery($('.article-gallery')[0], options);
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
+        })
     }
-    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-        var options = {
-            rowHeight: 140,
-            margins: 4,
-            lastRow: 'justify'
-        };
-        $('.justified-gallery').justifiedGallery(options);
+    var isPCInit = false;
+    var loadPC = function(){
+        require([yiliaConfig.rootUrl + 'js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
+        })
     }
 
-    // Profile card
-    var profileElem = $('#profile');
-    $(document).on('click', function () {
-        profileElem.removeClass('card');
-    }).on('click', '#profile-anchor', function (e) {
-        e.stopPropagation();
-        profileElem.toggleClass('card');
-    }).on('click', '.profile-inner', function (e) {
-        e.stopPropagation();
+    var browser = {
+        versions: function() {
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
+    }
+
+    $(window).bind("resize", function() {
+        if (isMobileInit && isPCInit) {
+            $(window).unbind("resize");
+            return;
+        }
+        var w = $(window).width();
+        if (w >= 700) {
+            loadPC();
+        } else {
+            loadMobile();
+        }
     });
 
-    // To Top
-    var sidebarElem = $('#sidebar'),
-        toTopElem = $('#toTop');
+    if(!!browser.versions.mobile || $(window).width() < 800){
+        loadMobile();
+    } else {
+        loadPC();
+    }
 
-    (function () {
-        if (!sidebarElem.length) return;
+    resetTags = function(){
+        var tags = $(".tagcloud a");
+        for(var i = 0; i < tags.length; i++){
+            var num = Math.floor(Math.random()*7);
+            tags.eq(i).addClass("color" + num);
+        }
+        $(".article-category a:nth-child(-n+2)").attr("class", "color0");
+    }
 
-        checkDisplayToTop();
-        $(document).on('scroll', function () {
-            checkDisplayToTop();
-        });
-
-        toTopElem.click(function () {
-            $('body, html').animate({ scrollTop: 0 }, 600);
-        });
-
-        var isToTopDisplayed = false;
-        function checkDisplayToTop() {
-            var toTop = (sidebarElem.height() - $(window).height()) + 60;
-            var currentScrollTop = $(document).scrollTop();
-            var needDisplay = (currentScrollTop > toTop) && (currentScrollTop > 0);
-
-            if ($(document).width() >= 800) {
-                if (needDisplay) {
-                    if (isToTopDisplayed) return;
-                    toTopElem.fadeIn();
-                    toTopElem.css('left', sidebarElem.offset().left);
-                    isToTopDisplayed = true;
-                } else {
-                    if (!isToTopDisplayed) return;
-                    toTopElem.fadeOut();
-                    isToTopDisplayed = false;
+    // fancyBox
+    if(!!yiliaConfig.fancybox){
+        require([yiliaConfig.fancybox_js], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    if(typeof(title) == "undefined"){
+                        var title = imgArr.eq(i).attr("title");
+                    }
+                    var width = imgArr.eq(i).attr("width");
+                    var height = imgArr.eq(i).attr("height");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' width="+width+" height="+height+" title='"+title+"' alt='"+title+"'></a>");
                 }
-            } else {
-                toTopElem.show();
-                toTopElem.css('right', 20);
+                $(".article-inner .fancy-ctn").fancybox({ type: "image" });
+            }
+        })
+    }
+
+    // Animate on Homepage
+    if(!!yiliaConfig.animate) {
+        if(!!yiliaConfig.isHome) {
+            require([yiliaConfig.scrollreveal], function (ScrollReveal) {
+                var animationNames = [
+                "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+                ],
+                len = animationNames.length,
+                randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
+                // Fallback (CSS3 keyframe, requestAnimationFrame)
+                if (!window.requestAnimationFrame) {
+                    $('.body-wrap > article').css({opacity: 1});
+                    if (navigator.userAgent.match(/Safari/i)) {
+                        function showArticle(){
+                            $(".article").each(function(){
+                                if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                                    $(this).removeClass("hidden").addClass("show");
+                                    $(this).addClass("is-hiddened");
+                                } else {
+                                    if(!$(this).hasClass("is-hiddened")) {
+                                        $(this).addClass("hidden");
+                                    }
+                                }
+                            })
+                        }
+                        $(window).on('scroll', function(){
+                            showArticle();
+                        });
+                        showArticle();
+                    }
+                    return;
+                }
+
+                var animateScope = ".body-wrap > article";
+                var $firstArticle = $(".body-wrap > article:first-child");
+                if ($firstArticle.height() > $(window).height()) {
+                    var animateScope = ".body-wrap > article:not(:first-child)";
+                    $firstArticle.css({opacity: 1});
+                }
+                ScrollReveal({
+                    duration: 0,
+                    afterReveal: function (domEl) {
+                        $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1})
+                    }
+                }).reveal(animateScope);
+            })
+        } else {
+            $('.body-wrap > article').css({opacity: 1});
+        }
+    }
+
+    // TOC
+    if (yiliaConfig.toc) {
+        require(['toc'], function(){ })
+    }
+
+    // Random Color 边栏顶部随机颜色
+    var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
+    var id = Math.ceil(Math.random()*(colorList.length-1));
+    // PC
+    $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
+    // Mobile
+    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
+
+    // Table
+    $("table").wrap("<div class='table-area'></div>");
+
+    // Hide Comment Button
+    $(document).ready(function() {
+        if ($("#comments").length < 1) {
+            $("#scroll > a:nth-child(2)").hide();
+        }
+    })
+
+    // Hide Labels
+    if(yiliaConfig.isArchive || yiliaConfig.isTag || yiliaConfig.isCategory) {
+        $(document).ready(function() {
+            $("#footer").after("<button class='hide-labels'>TAGS</button>");
+            $(".hide-labels").click(function() {
+                $(".article-info").toggle(200);
+            })
+        })
+    }
+
+    // Task lists in markdown
+    $('ul > li').each(function() {
+        var taskList = {
+            field: this.textContent.substring(0, 2),
+            check: function(str) {
+                var re = new RegExp(str);
+                return this.field.match(re);
             }
         }
-    })();
 
-    // Fixed Profile
-    (function () {
-        checkFixedProfile();
-        $(document).on('scroll', function () {
-            checkFixedProfile();
-        });
+        var string = ["[ ]", ["[x]", "checked"]];
+        var checked = taskList.check(string[1][0]);
+        var unchecked = taskList.check(string[0]);
 
-        var isFixedProfile = false;
-        function checkFixedProfile() {
-            if (!profileElem.is('.profile-fixed')) return;
-            if ($(document).width() < 800) return;
+        var $current = $(this);
+        function update(str, check) {
+            var click = ["disabled", ""];
+            $current.html($current.html().replace(
+              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+            )
+        }
 
-            var currentScrollTop = $(document).scrollTop();
-            var profileInnerElem = $('#profile .profile-inner');
-            var needFixed = currentScrollTop >= profileElem.offset().top + profileElem.outerHeight(true);
-
-            if (needFixed) {
-                if (isFixedProfile) return;
-
-                profileInnerElem.css('position', 'fixed')
-                    .css('width', profileElem.innerWidth() + 'px')
-                    .css('top', '0');
-
-                // css animation fade-in
-                profileInnerElem.css('animation', '');
-                profileInnerElem.addClass('anim-fade-in');
-                isFixedProfile = true;
+        if (checked || unchecked) {
+            this.classList.add("task-list");
+            if (checked) {
+                update(string[1][0], string[1][1]);
+                this.classList.add("check");
             } else {
-                if (!isFixedProfile) return;
-
-                profileInnerElem.css('position', '')
-                    .css('width', '')
-                    .css('top', '');
-
-                profileInnerElem.css('animation', 'none');
-                isFixedProfile = false;
+                update(string[0], "");
             }
         }
-    })();
+    })
 
-})(jQuery);
+})
